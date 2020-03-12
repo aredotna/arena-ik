b7a6c0ff1d4a716332bdb7db5a48de48aedd43a7
Charles Broskoski <broskoski@gmail.com>
2020-03-12 17:47:55 -0400

    Update styling and add archive

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,8 @@ import useAxios from "axios-hooks";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
 
-import wsfgImg from "./assets/WSFG_Title.png";
-
 import client from "apollo/index";
+import { FeelingArchive } from "components/FeelingArchive";
 
 const Container = styled.div`
   max-width: 60em;
@@ -15,45 +14,79 @@ const Container = styled.div`
   padding: 0 1em;
 `;
 
-const Header = styled.div`
+const Title = styled.div`
+  font-family: "SometimesTimes";
+  font-size: 4em;
+  color: white;
+  margin: 1em 0;
+  text-transform: uppercase;
+  font-weight: bold;
+`;
+
+const DefinitionContainer = styled.div`
+  display: flex;
+`;
+
+const Definition = styled.div`
+  font-size: 1.5em;
+  padding: 0 1em;
+  text-align: left;
+`;
+
+const Question = styled.div`
   font-size: 2em;
-  margin-bottom: 3em;
+  font-weight: bold;
+  padding: 1em 0 0;
 `;
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
+  align-items: center;
 `;
 
 const Textarea = styled.input.attrs({ type: "text", autocomplete: "no" })`
   display: block;
-  margin-bottom: 0.5em;
+  margin: 0.5em 0 1.5em;
   border: 0;
-  font-size: 6em;
+  font-size: 4em;
+  background: transparent;
+  text-align: center;
+  outline: none;
+  padding: 1em 0;
+  flex: 1 1 auto;
+  width: 100%;
+
+  border: 2px solid black;
+  border-radius: 40em;
+  border-top-color: transparent;
+  border-bottom-color: transparent;
+
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.5);
+  }
 `;
 
 const Button = styled.input.attrs({ type: "submit" })`
-  font-size: 3em;
+  font-size: 2.5em;
   border: 0;
-  padding: 0.25em;
+  padding: 0.5em 2em;
+  flex: 0 0 auto;
+  border-radius: 20em;
+  background-color: transparent;
+  border: 2px solid black;
+  cursor: pointer;
+  transition: background-color 0.5s ease, border-color 0.5s ease;
+
+  &:hover {
+    background-color: white;
+    border: 2px solid white;
+  }
 `;
 
 type FormData = {
   content: string;
 };
-
-const possiblePrompts = [
-  "Write one word to describe the feeling that you get when someone whispers in your ear.",
-  "Write one word to describe the feeling that you get when someone taps their fingers on a table.",
-  "Write one word to describe the feeling that you get when an optician shines a light in your eye.",
-  "Write one word to describe the feeling that you get when someone turns the page of a book in a library.",
-  "Write one word to describe the feeling that you get when someone applies make-up to their face.",
-  "Write one word to describe the feeling that you get when someone concentrates intensely.",
-  "Write one word to describe the feeling that you get when someone is wrapping a gift."
-];
-
-const prompt =
-  possiblePrompts[Math.floor(Math.random() * possiblePrompts.length)];
 
 const App: React.FC = () => {
   const [mode, setMode] = useState<
@@ -91,13 +124,48 @@ const App: React.FC = () => {
   return (
     <ApolloProvider client={client}>
       <Container>
-        <img src={wsfgImg} alt="Weird Sensation Feels Good" width="200px" />
-        <Header>{prompt}</Header>
+        <Title>Weird Sensation Feels Good</Title>
+        <DefinitionContainer>
+          <Definition>
+            <p>
+              <strong>Trigger</strong>
+            </p>
+
+            <p>
+              A sound or action that instils in you an Autonomous Sensory
+              Meridian Response (ASMR).
+            </p>
+
+            <p>
+              Everything has the potential to be a trigger, but there is no such
+              thing as an objective trigger.
+            </p>
+          </Definition>
+          <Definition>
+            <p>
+              <strong>Trigger</strong>
+            </p>
+
+            <p>
+              Ett ljud eller en handling som inger en ASMR-känsla hos någon
+              annan.
+            </p>
+
+            <p>
+              Allt har potential att vara en trigger, men det finns samtidigt
+              inga objektiva triggers.
+            </p>
+          </Definition>
+        </DefinitionContainer>
+        <Question>
+          Describe an experience that has triggered an ASMR response.
+        </Question>
         <Form onSubmit={handleSubmit(onSubmit)}>
           <Textarea
             name="content"
             ref={register({ required: true, maxLength: 20 })}
             autoComplete="no"
+            placeholder="Type something..."
           />
           <Button
             disabled={mode !== "resting"}
@@ -112,6 +180,7 @@ const App: React.FC = () => {
             }
           />
         </Form>
+        <FeelingArchive />
       </Container>
     </ApolloProvider>
   );
---
+++ client/src/components/FeelingArchive/index.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
+import { gql } from "apollo-boost";
+
+const Container = styled.div`
+  margin: 8em 0;
+`;
+
+const Header = styled.div`
+  font-family: "SometimesTimes";
+  font-size: 4em;
+  color: white;
+  margin: 1em 0;
+  text-transform: uppercase;
+  font-weight: bold;
+`;
+
+const FEELING_QUERY = gql`
+  {
+    channel(id: "archive-of-feelings") {
+      blokks(type: TEXT) {
+        __typename
+        ... on Text {
+          id
+          content(format: MARKDOWN)
+        }
+      }
+    }
+  }
+`;
+
+export const FeelingArchive: React.FC = () => {
+  const { loading, error, data } = useQuery(FEELING_QUERY);
+  console.log("process.env", process.env);
+  return (
+    <Container>
+      <Header>Archive of feelings</Header>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>{error}</h1>}
+      {data &&
+        data.channel &&
+        data.channel.blokks.map((block: any) => {
+          return <h1>{block.content}</h1>;
+        })}
+    </Container>
+  );
+};
