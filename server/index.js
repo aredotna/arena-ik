e60bd2650ba22491ba1834512ca5c8e9fc4eea3f
Charles Broskoski <broskoski@gmail.com>
2020-04-23 10:26:08 -0400

    Trust forwarded head

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan("combined"));
 app.use(sslRedirect());
-app.use(enforce.HTTPS());
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.get("/api/policy", (req, res) => {
   axios({
